52f0d3ce622e148b8fd12009e4208789b9811d51
Camilo Cruz <juan.cruz37552@ucaldas.edu.co>
2024-04-14 13:02:53 -0500

    Creación modelo de Usuario

---
+++ src/models/usuario.model.ts
@@ -0,0 +1,80 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Usuario extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  primerNombre: string;
+
+  @property({
+    type: 'string',
+  })
+  segundoNombre?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  primerApellido: string;
+
+  @property({
+    type: 'string',
+  })
+  segundoApellido?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  lugarResidencia: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  correo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  telefono: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  clave: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fechaCreacion: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  modoRecuperacionCuenta: string;
+
+
+  constructor(data?: Partial<Usuario>) {
+    super(data);
+  }
+}
+
+export interface UsuarioRelations {
+  // describe navigational properties here
+}
+
+export type UsuarioWithRelations = Usuario & UsuarioRelations;
