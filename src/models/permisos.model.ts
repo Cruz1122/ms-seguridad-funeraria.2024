99e279fb24762a7f3ffb1269f09994d4d459e241
Jhonder18 <jhon.patino29550@ucaldas.edu.co>
2024-04-14 13:11:37 -0500

    Creacion modelo Permisos

---
+++ src/models/permisos.model.ts
@@ -0,0 +1,33 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Permisos extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+  })
+  comentarios?: string;
+
+
+  constructor(data?: Partial<Permisos>) {
+    super(data);
+  }
+}
+
+export interface PermisosRelations {
+  // describe navigational properties here
+}
+
+export type PermisosWithRelations = Permisos & PermisosRelations;
