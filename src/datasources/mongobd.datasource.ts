03c7d9d610c57bc99bb5331df539cf2813b3630f
Camilo Cruz <juan.cruz37552@ucaldas.edu.co>
2024-04-14 15:38:26 -0500

    Actualización en datasource

--- src/datasources/mongobd.datasource.ts
+++ src/datasources/mongobd.datasource.ts
@@ -18,8 +18,10 @@ const config = {
 // gracefully. The `stop()` method is inherited from `juggler.DataSource`.
 // Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
-export class MongobdDataSource extends juggler.DataSource
-  implements LifeCycleObserver {
+export class MongobdDataSource
+  extends juggler.DataSource
+  implements LifeCycleObserver
+{
   static dataSourceName = 'mongobd';
   static readonly defaultConfig = config;
 
